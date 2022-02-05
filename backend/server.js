fe9db812ca7d28555396382990586f12f7fedfa3
Muhammad Samiullah <msami398@gmail.com>
2022-02-06 04:20:55 +0500

    setup: configure basic route & connect backend with the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { chats } = require("./data/dummy-data");
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,13 @@ app.get("/", (req, res) => {
   res.send("home route");
 });
 
-app.get("");
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
 
 app.listen(PORT, console.log(`server has started on PORT ${PORT}`));
