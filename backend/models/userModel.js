f91c9ba77b9abdfc9b2365c04108c9ec50173160
Muhammad Samiullah <msami398@gmail.com>
2022-02-08 15:20:40 +0500

    feature: added schema for users, messages & chats

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.Model("User", userSchema);
+module.exports = User;
